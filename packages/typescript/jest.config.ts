e8e22c3ac2072e860e146dbed19cb246286a4931
Niclas Roßberger <niclas@nidomiro.de>
2022-10-30 12:28:19 +0100

    refactor: renames package

--- packages/typescript/jest.config.ts
+++ packages/typescript/jest.config.ts
@@ -1,6 +1,6 @@
 /* eslint-disable */
 export default {
-	displayName: 'relation-tuple-parser',
+	displayName: 'typescript',
 	preset: '../../jest.preset.js',
 	globals: {
 		'ts-jest': {
@@ -12,5 +12,5 @@ export default {
 		'^.+\\.[tj]s$': 'ts-jest',
 	},
 	moduleFileExtensions: ['ts', 'js', 'html'],
-	coverageDirectory: '../../coverage/packages/relation-tuple-parser',
+	coverageDirectory: '../../coverage/packages/typescript',
 }
